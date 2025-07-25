75bb146f274f87495fdd8d00f96f7f9ab6430756
Marzuuq <marzooqkinya22@gmail.com>
2025-07-25 20:13:48 +0530

    added hover effect

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const myProjects = [
 ];
 myProjects.forEach((item)=>{
     let card = document.createElement("div");
-    card.className="shadow-sm rounded-sm   w-90  md:w-65vw]   p-10 m-3 mt-4"
+    card.className="shadow-sm rounded-sm   w-90  md:w-65vw]   p-10 m-3 mt-4  transition-all duration-300 hover:shadow-lg hover:scale-[1.02] bg-white"
     card.innerHTML = `<img src = "${item.img} " class = "h-16 m-auto rounded-xl mb-0.5 "> <h4 class = " text-xl font-bold mt-2"> ${item.name} </h4> <p class = "text-[1em] max-w-[15vw] m-auto mb-2 mt-2"> ${item.desc}</p>
     <a  class = " text-blue-800" href ="${item.link}" target = "blank" > Github Link </a>`;
     projectDiv.appendChild (card);
@@ -26,7 +26,7 @@ const learned = [
 
 learned.forEach((item1) => {
   let cardLearn = document.createElement("div");
-  cardLearn.className = "shadow-sm rounded-sm   w-90  md:w-65vw]   p-10 m-3 mt-13"
+  cardLearn.className = "shadow-sm rounded-sm   w-90  md:w-65vw]   p-10 m-3 mt-13 transition-all duration-300 hover:shadow-lg hover:scale-[1.02] bg-white"
   cardLearn.innerHTML = `<img src = "${item1.img}" class = "h-16  m-auto mb-5" > <h4 class = "text-xl">  ${item1.name} </h4> <p class = "text-[0.7em]"> ${item1.desc} </p>`;
   projectLearn.appendChild(cardLearn);
 }) 
