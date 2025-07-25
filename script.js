22fddd1618f807128565b0ea95b397bb4b37fcac
Marzuuq <marzooqkinya22@gmail.com>
2025-07-25 20:01:19 +0530

    Added Projects

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const projectDiv = document.getElementById("projects");
 const myProjects = [
-  { name: "Lorem2", desc: "Lorem10" },
-  { name: "Lorem2", desc: "Lorem10" },
-  { name: "Lorem2", desc: "Lorem10" },
+  { name: "Age Calculator", desc: "This is a fundamental project for me to practice" , img: "./Assets/Age.png" , link: "https://github.com/Marzuuq/My-Frontend-projects/tree/main/Age-calculator" },
+  { name: "Mini Calculator", desc: "This is a fundamental project for me to hone my skills." , img:"./Assets/mathematics.png" , link:"https://github.com/Marzuuq/My-Frontend-projects/tree/main/mini-calculator" },
+  ,
 ];
 myProjects.forEach((item)=>{
     let card = document.createElement("div");
-    card.className="shadow-sm rounded-sm   w-90  md:w-65vw]   p-10 m-3 mt-13 "
-    card.innerHTML = `<h4 class = " text-xl"> ${item.name} </h4> <p class = "text-[0.5em]"> ${item.desc}</p>`;
+    card.className="shadow-sm rounded-sm   w-90  md:w-65vw]   p-10 m-3 mt-4"
+    card.innerHTML = `<img src = "${item.img} " class = "h-16 m-auto rounded-xl mb-0.5 "> <h4 class = " text-xl font-bold mt-2"> ${item.name} </h4> <p class = "text-[1em] max-w-[15vw] m-auto mb-2 mt-2"> ${item.desc}</p>
+    <a  class = " text-blue-800" href ="${item.link}" target = "blank" > Github Link </a>`;
     projectDiv.appendChild (card);
 }
 )
@@ -20,7 +21,7 @@ const projectLearn = document.getElementById("LearningPaths");
 const learned = [
   {name: "Learned Java" , desc:"Very Basic" , img:"./Assets/java.png"},
   {name: "Learned C" , desc: "Very Basic" , img: "./Assets/c.png"},
-  {name: "Learned JS" , desc: "Very Bsic" , img:"./Assets/javascript.png"}
+  {name: "Learned JS" , desc: "Very Basic" , img:"./Assets/javascript.png"}
 ];
 
 learned.forEach((item1) => {
